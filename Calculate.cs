40a68a4342e3bb4bc5f90b6798324c2c69295ca4
Dikgupta <somya.1345@gmail.com>
2017-02-17 08:59:19 +0530

    Add files via upload

---
+++ Calculate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class Calculate
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleDimensionArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = new int[5];
+
+            Console.Write("Enter Array Element : ");
+
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                intArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Array Elements Are : ");
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                Console.Write(intArr[i] + "\t");
+            }
+            Console.WriteLine();
+
+            Array.Sort(intArr);
+
+            Console.WriteLine("Sorted Array is : ");
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                Console.Write(intArr[i] + "\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
